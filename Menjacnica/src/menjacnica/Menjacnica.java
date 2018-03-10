6893a645c5b5c9558f3565ff2595d95976e40f45
MiljanSimonovic <miljans1977@gmail.com>
2018-03-10 01:22:46 +0100

    Kreirana klasa Menjacnica

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(String nazivValute, String vrsta, GregorianCalendar datum, double vrednost) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisiKurs(String nazivValute, String vrsta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs vratiKurs(String nazivValute, String vrsta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
